dae3c4d534337bbde618de140ab72f30d88db9da
Christian-GL <christiangl202@hotmail.com>
2025-01-05 15:32:28 +0100

    Añadido SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 
-import { useState } from 'react'
-import './App.css'
-
-
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
 
   return (
-    <>
-      <div>
-        <p>ola</p>
-      </div>
-    </>
+    <div>
+      <p>Soy App.jsx</p>
+    </div>
   )
-}
 
-export default App
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
-import './main.css'
-import App from './App.jsx'
+import { App } from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
